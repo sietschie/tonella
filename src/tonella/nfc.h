41e1e6702b68c1fc61a61e7df98a78c36cb4366e
SieTschie <sietschie@gmail.com>
2020-03-25 20:19:25 +0100

    initial implementation of ndef format

--- src/tonella/nfc.h
+++ src/tonella/nfc.h
@@ -4,6 +4,6 @@
 #define NFC_TAG_FOUND 1
 #define NFC_TAG_GONE 2
 
-bool readCard(byte *index);
-byte checkCardStatus(byte *index);
+bool readCard(char &type, int &index);
+byte checkCardStatus(char &type, int &index);
 bool nfc_init();
